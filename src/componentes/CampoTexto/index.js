e1c8f3ce62b066ee928061cf2bb2e01314d71f62
Giovanne Piçuti <picuti256@gmail.com>
2022-11-16 08:52:39 -0300

    Ajuste no organo

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,22 @@
+import './campoTexto.css'
+
+const CampoTexto = (props) => {
+
+    const placeholderModificada = `${props.placeholder}`
+
+   
+
+    const aoDigitado = (e) => {
+        props.aoAlterado(e.target.value)
+    }
+
+
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
+        </div>
+    )
+}
+
+export default CampoTexto
